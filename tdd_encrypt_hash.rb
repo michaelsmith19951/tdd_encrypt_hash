c2210ded90bf5f98835e91c28b76dafb1f22dd93
Michael Smith <michaelsmith19951@yahoo.com>
2018-07-02 16:01:09 -0400

    3rd test that failed, test_if_letter_position_is_correct

--- tdd_encrypt_hash.rb
+++ tdd_encrypt_hash.rb
@@ -1,9 +1,12 @@
 # All of the letters, inside a hash
 
 
-# Function for all of the letters
+# Function for all of the letters, as an array
 	def letter(alphabet)
-	letters = {"a" => 1, "b" => 2, "c" => 3, "d" => 4, "e" => 5, "f" => 6, "g" => 7, "h" => 8, "i" => 9, "j" => 10, "k" => 11, "l" => 12, "m" => 13, "n" => 14, "o" => 15, "p" => 16, "q" => 17, "r" => 18, "s" => 19, "t" => 20, "u" => 21, "v" => 22, "w" => 23, "x" => 24, "y" => 25, "z" => 26}
-	letters.values
+	letters = ("a".."z").to_a
+	letters_shifted = ("f".."z").to_a
+	letters_right_end = ("a".."e").to_a
+	letters_shifted + letters_right_end
+
 	end
 
--- test_tdd_encrypt_hash.rb
+++ test_tdd_encrypt_hash.rb
@@ -8,7 +8,13 @@ require_relative "tdd_encrypt_hash.rb"
 class TestTddEncryptHash < Minitest::Test
 
 # Functions for each test
-	def test_if_hash_is_class
+	def test_if_array_is_class
 		assert_equal(Array, letter({}).class)
+	end
+
+
+	def test_if_letter_position_is_correct
+		letters = [*"a".."z"]
+		assert_equal(letters[1], "g")
 	end
 end
